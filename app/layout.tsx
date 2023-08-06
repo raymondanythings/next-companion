b19a162c91fbb2a9ea44849a62d1c129a3a5bab1
Raymond <akdfhr2@gmail.com>
2023-08-06 15:19:28 +0900

    TODO: DB연결 진행중

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import ThemeProvider from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body>
+        <body className={cn("bg-secondary")}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
           </ThemeProvider>
---
+++ components/search-input.tsx
@@ -0,0 +1,54 @@
+"use client";
+import qs from "query-string";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const searchParamas = useSearchParams();
+
+  const categoryId = searchParamas.get("categoryId");
+  const name = searchParamas.get("name");
+
+  const [value, setValue] = useState(name || "");
+  const debouncedValue = useDebounce(value, 500);
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    console.log(url);
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
+
+  return (
+    <div className="relative">
+      <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+      <Input
+        onChange={onChange}
+        value={value}
+        placeholder="Search.."
+        className="pl-10 bg-primary/10"
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
